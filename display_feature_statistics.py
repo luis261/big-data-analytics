d5181200b16c9e0884e91fae8c1d6dd87e2a73e3
luis261 <luis.goerler@gmx.de>
2021-09-06 13:00:32 +0200

    Add statistics of the remaining features

--- display_feature_statistics.py
+++ display_feature_statistics.py
@@ -11,7 +11,13 @@ def main():
     plt.bar(x, y)
     plt.show()
     print("These are the relative frequencies of male and female customers: " + "Male: " + str(gender_counted[0]/data.shape[0]) + " Female: " + str(gender_counted[1]/data.shape[0]))
-    # TODO other features as well
+
+    plt.boxplot(data["Age"], labels = ["Age"])
+    plt.show()
+    plt.boxplot(data["Annual Income (k$)"], labels = ["Annual Income (k$)"])
+    plt.show()
+    plt.boxplot(data["Spending Score (1-100)"], labels = ["Spending Score (1-100)"])
+    plt.show()
 
 
 if __name__ == "__main__":
