5b545d5fbf2993240a0344c48da58df946f1a2a6
luis261 <luis.goerler@gmx.de>
2021-09-06 18:41:13 +0200

    Refactor code, show result for k = 3

--- cluster.py
+++ cluster.py
@@ -5,6 +5,49 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
 
+class ColorException(Exception):
+    pass
+
+def cluster_data_and_show(data, k):
+    if k > 5:
+        raise ColorException("k exceeds amount of available colors")
+    else:
+        model = KMeans(n_clusters = k)
+        labels = model.fit_predict(data)
+        labels = pd.DataFrame(labels, columns=["cluster"])
+        clustered_data = pd.concat([data, labels], axis= 1)
+        clusters = []
+        for i in range(k):
+            clusters.append(clustered_data[clustered_data["cluster"] == i])
+
+        # separate customers by gender to be able to dispay them differently
+        male_points_per_cluster = []
+        female_points_per_cluster = []
+        for cluster in clusters:
+            male_points_per_cluster.append(cluster[cluster["Gender"] == 0])
+            female_points_per_cluster.append(cluster[cluster["Gender"] == 1])
+        clusters_separated_by_gender = [male_points_per_cluster, female_points_per_cluster]
+        colors = ["#f23c14", "#72f214", "#14f2e5", "#f214cd", "#2c14f2"]
+        markers = ["o", "x"]
+
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        for i in range(len(clusters_separated_by_gender[0])):
+            customers_per_gender = {}
+            for j in range(len(clusters_separated_by_gender)):
+                gender = "male" if j == 0 else "female"
+                try:
+                    customers_per_gender[gender] += clusters_separated_by_gender[j][i].shape[0]
+                except KeyError:
+                    customers_per_gender[gender] = clusters_separated_by_gender[j][i].shape[0]
+                ax.scatter(clusters_separated_by_gender[j][i]["Age"], clusters_separated_by_gender[j][i]["Annual Income (k$)"], clusters_separated_by_gender[j][i]["Spending Score (1-100)"], c = colors[i], marker = markers[j])
+            print("The cluster with the color " + colors[i] + " has " + str((100 * customers_per_gender["male"])/(customers_per_gender["male"] + customers_per_gender["female"])) + "% male customers.")
+        ax.set_xlabel("Age")
+        ax.set_ylabel("Annual Income (k$)")
+        ax.set_zlabel("Spending Score (1-100)")
+        plt.show()
+
+
 def main():
     data = pd.read_pickle("transformed_data.pkl")
 
@@ -39,10 +82,6 @@ def main():
     plt.bar(range(2, 10), averaged_scores)
     plt.show()
 
-    model = KMeans(n_clusters = 5)
-    labels = model.fit_predict(data)
-    labels = pd.DataFrame(labels, columns=["cluster"])
-    clustered_data = pd.concat([data, labels], axis= 1)
 
     fig = plt.figure()
     ax = Axes3D(fig)
@@ -52,39 +91,8 @@ def main():
     ax.set_zlabel("Spending Score (1-100)")
     plt.show()
 
-    clusters = []
-    clusters.append(clustered_data[clustered_data["cluster"] == 0])
-    clusters.append(clustered_data[clustered_data["cluster"] == 1])
-    clusters.append(clustered_data[clustered_data["cluster"] == 2])
-    clusters.append(clustered_data[clustered_data["cluster"] == 3])
-    clusters.append(clustered_data[clustered_data["cluster"] == 4])
-
-    # separate customers by gender to be able to dispay them differently
-    male_points_per_cluster = []
-    female_points_per_cluster = []
-    for cluster in clusters:
-        male_points_per_cluster.append(cluster[cluster["Gender"] == 0])
-        female_points_per_cluster.append(cluster[cluster["Gender"] == 1])
-    clusters_separated_by_gender = [male_points_per_cluster, female_points_per_cluster]
-    colors = ["#f23c14", "#72f214", "#14f2e5", "#f214cd", "#2c14f2"]
-    markers = ["o", "x"]
-
-    fig = plt.figure()
-    ax = Axes3D(fig)
-    for i in range(len(clusters_separated_by_gender[0])):
-        customers_per_gender = {}
-        for j in range(len(clusters_separated_by_gender)):
-            gender = "male" if j == 0 else "female"
-            try:
-                customers_per_gender[gender] += clusters_separated_by_gender[j][i].shape[0]
-            except KeyError:
-                customers_per_gender[gender] = clusters_separated_by_gender[j][i].shape[0]
-            ax.scatter(clusters_separated_by_gender[j][i]["Age"], clusters_separated_by_gender[j][i]["Annual Income (k$)"], clusters_separated_by_gender[j][i]["Spending Score (1-100)"], c = colors[i], marker = markers[j])
-        print("The cluster with the color " + colors[i] + " has " + str((100 * customers_per_gender["male"])/(customers_per_gender["male"] + customers_per_gender["female"])) + "% male customers.")
-    ax.set_xlabel("Age")
-    ax.set_ylabel("Annual Income (k$)")
-    ax.set_zlabel("Spending Score (1-100)")
-    plt.show()
+    cluster_data_and_show(data, 3)
+    cluster_data_and_show(data, 5)
 
 
 if __name__ == "__main__":
