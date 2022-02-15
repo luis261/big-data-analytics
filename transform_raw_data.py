3737c715f8781276523503f1733189a4ab0c353e
luis261 <luis.goerler@gmx.de>
2022-02-15 11:49:22 +0100

    Add scaling

--- transform_raw_data.py
+++ transform_raw_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
 
 
 def main():
@@ -9,8 +10,13 @@ def main():
     data = pd.read_csv("raw_data/mall_customers.csv", decimal = ",")
     data["Gender"] = data["Gender"].map({"Female": 1, "Male": 0})
 
+    columns_to_scale = list(data.columns)
+    columns_to_scale.pop(0)
+    data[columns_to_scale] = MinMaxScaler().fit_transform(data[columns_to_scale])
+
     print("Transformed data:")
     print(data)
+
     data.to_pickle(file_name)
     print("\nPersisted the transformed data in the file \"" + file_name + "\"")
 
